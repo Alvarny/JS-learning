ae484c9154ffdcc32bc093262e876896ec165365
Alvarny <30897762+Alvarny@users.noreply.github.com>
2021-01-19 15:31:14 +0100

    Learned event listeners by opening and closing popups

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,42 @@
 'use strict';
+
+// Reference to DOM elements
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// Helper functions
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+// Loop over buttons to add event listeners (to open popup)
+for (let i = 0; i < btnsOpenModal.length; i++) {
+
+    let btnOpenModal = btnsOpenModal[i];
+    console.log(btnOpenModal);
+
+    // Add event listener to remove 'hidden' class from modal window and overlay
+    btnOpenModal.addEventListener('click', openModal);
+}
+
+
+
+// Close popup
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+
+    // Check for ESC key and hidden class
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+
+});
+
